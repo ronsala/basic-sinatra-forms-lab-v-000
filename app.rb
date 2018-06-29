9f8203aeb904f0d44d03fa653b07a55d28236477
Ron Sala <ron.sala@gmail.com>
2018-06-29 13:57:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,10 +8,12 @@ class App < Sinatra::Base
 
   post '/team' do
     @name = params[:name]
-    # @name = params[:name]
-    # @name = params[:name]
-    # @name = params[:name]
-    # @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
     erb :team
   end
 
